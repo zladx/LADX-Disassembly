7c51a1649439c950aa370f408d3924368066df5d
Pierre de La Morinerie <kemenaran@gmail.com>
2016-08-16 20:58:18 +0200

    Remove unused constants and macros

--- src/constants/constants.asm
+++ src/constants/constants.asm
@@ -1,6 +1,3 @@
-; rst vector addresses are defined here.
-Bankswitch  equ 0
-FarCall     equ 8
 
 include "constants/gbhw.asm"
 include "constants/macros.asm"
--- src/constants/macros.asm
+++ src/constants/macros.asm
@@ -1,8 +1,3 @@
-farcall: macro
-	rst FarCall
-	db  bank(\1)
-	dw  \1
-endm
 
 RGB: macro
 	db (\1) + (\2) << 5 + (\3) << 10

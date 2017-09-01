8ca5c59c7511a6414c1bd58e75a47339c0192b7b
Pierre de La Morinerie <kemenaran@gmail.com>
2017-09-01 18:22:51 +0530

    clear_memory: move `ClearMemory` functions to their own file

--- src/code/bank0.asm
+++ src/code/bank0.asm
@@ -6528,53 +6528,7 @@ UpdateNextBGColumnWithTiles::
     jr   nz, UpdateNextBGColumnWithTiles
     ret
 
-; Clear $1600 bytes of WRAM (from $C000 to $D600)
-ClearLowerAndMiddleWRAM::
-    ld   bc, $1600
-    jr   ClearWRAMBytes
-
-; Clear $1300 bytes of WRAM (from $C000 to $D300)
-ClearLowerWRAM::
-    ld   bc, $1300
-    jr   ClearWRAMBytes
-
-; Clear lower values of HRAM (from $FF90 to $FFBF) and all WRAM
-ClearWRAMAndLowerHRAM::
-    ld   bc, $002F
-    jr   ClearHRAMBytesAndWRAM
-
-; Clear all values from HRAM and WRAM
-; (only `hIsGBC` is kept)
-ClearHRAMAndWRAM::
-    ; Set all bytes of HRAM (from $FF90 to $FFFD) to zero
-    ld   bc, $006D
-
-ClearHRAMBytesAndWRAM
-    ; Set BC bytes of HRAM (starting from $FF90) to zero
-    ld   hl, hGameValuesSection
-    call ClearBytes
-    ; Set all bytes of WRAM (from $C000 to $DF00) to zero
-    ld   bc, $1F00
-
-; Set BC bytes of WRAM (starting from $C000) to zero
-ClearWRAMBytes::
-    ld   hl, wram0Section
-
-; Set BC bytes of memory starting from HL to zero
-ClearBytes::
-    ldh  a, [hIsGBC]
-    push af
-
-.ClearBytes_loop
-    xor  a
-    ldi  [hl], a
-    dec  bc
-    ld   a, b
-    or   c
-    jr   nz, .ClearBytes_loop
-    pop  af
-    ldh  [hIsGBC], a
-    ret
+include "src/code/home/clear_memory.asm"
 
 label_29ED::
     ld   a, $14

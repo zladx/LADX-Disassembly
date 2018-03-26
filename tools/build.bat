e990221f034ea137b187a399f96db904f1e1351c
Pierre de La Morinerie <kemenaran@gmail.com>
2018-03-26 06:54:51 +0000

    tools: remove outdated build.bat

--- tools/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
-
-cl -nologo -FC -Wall -wd4820 -wd4710 -Zi -D_CRT_SECURE_NO_WARNINGS Z80Dis.c
-cl -nologo -FC -Wall -wd4820 -wd4710 -Zi -D_CRT_SECURE_NO_WARNINGS DumpBanks.c
-

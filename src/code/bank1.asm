1380fb926545af0209cf453a26172c0cd8e9f0b5
Pierre de La Morinerie <kemenaran@gmail.com>
2018-04-27 23:13:36 +0800

    bank1: move the face shrine mural to its own file

--- src/code/bank1.asm
+++ src/code/bank1.asm
@@ -5101,6 +5101,7 @@ OpenDungeonNameDialog::
 include "code/marin_beach.asm"
 
 PeachPictureEntryPoint::
+    ; Actually code for a jump table
     db $FA, $96, $DB, $C7, 8, $68, $29, $68, $56, $68, $73, $68, $AA, $68, $C0, $68
     db $25, $58, $E4, $68, 8, $69, $45, $69, $22, $58, $CD, $D6, $44
 
@@ -5484,115 +5485,7 @@ label_6AF4::
     call label_3CE6
     ret
 
-FaceShrineMuralEntryPoint::
-    ld   a, [wGameplaySubtype]
-    JP_TABLE
-._0 dw label_6B0A
-._1 dw label_6B2B
-._2 dw label_6B52
-._3 dw label_6B6F
-._4 dw label_6B81
-._5 dw label_6B9A
-._6 dw FileSaveFadeOut
-
-label_6B0A::
-    call IncrementGameplaySubtype
-    ldh  a, [hIsGBC]
-    and  a
-    jr   z, label_6B2B
-    ld   hl, $DC10
-    ld   c, $80
-    di
-
-label_6B18::
-    xor  a
-    ld   [rSVBK], a
-    ld   b, [hl]
-    ld   a, $03
-    ld   [rSVBK], a
-    ld   [hl], b
-    inc  hl
-    dec  c
-    ld   a, c
-    and  a
-    jr   nz, label_6B18
-    xor  a
-    ld   [rSVBK], a
-    ei
-
-label_6B2B::
-    ld   a, $01
-    ld   [$C167], a
-    call label_1A22
-    ld   a, [$C16B]
-    cp   $04
-    jr   nz, label_6B51
-    call label_5888
-    ld   a, $03
-    ldh  [hWindowYUnused], a
-    ld   a, $30
-    ldh  [hWindowXUnused], a
-    call IncrementGameplaySubtype
-    xor  a
-    ld   [$C1BF], a
-    ld   a, $14
-    ld   [wTileMapToLoad], a
-
-label_6B51::
-    ret
-
-label_6B52::
-    ld   a, $15
-    ld   [wBGMapToLoad], a
-    ld   a, $FF
-    ld   [wWindowY], a
-    xor  a
-    ldh  [hBaseScrollX], a
-    ldh  [$FF97], a
-    ld   [$C16B], a
-    ld   [$C16C], a
-    ld   a, $01
-    ld   [$DDD5], a
-    jp   IncrementGameplaySubtypeAndReturn
-
-label_6B6F::
-    call label_1A39
-    ld   a, [$C16B]
-    cp   $04
-    jr   nz, label_6B80
-    call IncrementGameplaySubtype
-    xor  a
-    ld   [$C3C4], a
-
-label_6B80::
-    ret
-
-label_6B81::
-    ld   a, [$C19F]
-    and  a
-    ret  nz
-    ld   a, [$C3C4]
-    inc  a
-    ld   [$C3C4], a
-    jp   z, IncrementGameplaySubtype
-    cp   $80
-    jr   nz, label_6B99
-    ld   a, $E7
-    call OpenDialog
-
-label_6B99::
-    ret
-
-label_6B9A::
-    ldh  a, [$FFCC]
-    and  $B0
-    jr   z, label_6BA7
-    ld   a, JINGLE_VALIDATE
-    ldh  [hJingle], a
-    call label_68D9
-
-label_6BA7::
-    ret
+include "code/face_shrine_mural.asm"
 
 label_6BA8::
     ldh  a, [$FFCC]
---
+++ src/code/face_shrine_mural.asm
@@ -0,0 +1,113 @@
+;
+; Cinematic sequence when reading the Face Shrine mural
+;
+
+FaceShrineMuralEntryPoint::
+    ld   a, [wGameplaySubtype]
+    JP_TABLE
+._0 dw label_6B0A
+._1 dw label_6B2B
+._2 dw label_6B52
+._3 dw label_6B6F
+._4 dw label_6B81
+._5 dw label_6B9A
+._6 dw FileSaveFadeOut
+
+label_6B0A::
+    call IncrementGameplaySubtype
+    ldh  a, [hIsGBC]
+    and  a
+    jr   z, label_6B2B
+    ld   hl, $DC10
+    ld   c, $80
+    di
+
+label_6B18::
+    xor  a
+    ld   [rSVBK], a
+    ld   b, [hl]
+    ld   a, $03
+    ld   [rSVBK], a
+    ld   [hl], b
+    inc  hl
+    dec  c
+    ld   a, c
+    and  a
+    jr   nz, label_6B18
+    xor  a
+    ld   [rSVBK], a
+    ei
+
+label_6B2B::
+    ld   a, $01
+    ld   [$C167], a
+    call label_1A22
+    ld   a, [$C16B]
+    cp   $04
+    jr   nz, label_6B51
+    call label_5888
+    ld   a, $03
+    ldh  [hWindowYUnused], a
+    ld   a, $30
+    ldh  [hWindowXUnused], a
+    call IncrementGameplaySubtype
+    xor  a
+    ld   [$C1BF], a
+    ld   a, $14
+    ld   [wTileMapToLoad], a
+
+label_6B51::
+    ret
+
+label_6B52::
+    ld   a, $15
+    ld   [wBGMapToLoad], a
+    ld   a, $FF
+    ld   [wWindowY], a
+    xor  a
+    ldh  [hBaseScrollX], a
+    ldh  [$FF97], a
+    ld   [$C16B], a
+    ld   [$C16C], a
+    ld   a, $01
+    ld   [$DDD5], a
+    jp   IncrementGameplaySubtypeAndReturn
+
+label_6B6F::
+    call label_1A39
+    ld   a, [$C16B]
+    cp   $04
+    jr   nz, label_6B80
+    call IncrementGameplaySubtype
+    xor  a
+    ld   [$C3C4], a
+
+label_6B80::
+    ret
+
+label_6B81::
+    ld   a, [$C19F]
+    and  a
+    ret  nz
+    ld   a, [$C3C4]
+    inc  a
+    ld   [$C3C4], a
+    jp   z, IncrementGameplaySubtype
+    cp   $80
+    jr   nz, label_6B99
+    ld   a, $E7
+    call OpenDialog
+
+label_6B99::
+    ret
+
+label_6B9A::
+    ldh  a, [$FFCC]
+    and  $B0
+    jr   z, label_6BA7
+    ld   a, JINGLE_VALIDATE
+    ldh  [hJingle], a
+    call label_68D9
+
+label_6BA7::
+    ret

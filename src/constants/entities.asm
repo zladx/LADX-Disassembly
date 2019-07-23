56546d21f7a808a169dc3ded05a25e136526c23f
Pierre de La Morinerie <kemenaran@gmail.com>
2019-07-23 23:15:54 +0200

    entities: better document the value of constants

--- src/constants/entities.asm
+++ src/constants/entities.asm
@@ -1,12 +1,13 @@
 ; Values for wEntitiesStateTable
 ENTITY_STATE_DISABLED   equ 0
-ENTITY_STATE_1          equ 1
-ENTITY_STATE_2          equ 2
+ENTITY_STATE_DYING      equ 1
+ENTITY_STATE_FALLING    equ 2
 ENTITY_STATE_DESTROYING equ 3
-ENTITY_STATE_4          equ 4
+ENTITY_STATE_INIT       equ 4
 ENTITY_STATE_ACTIVE     equ 5
-ENTITY_STATE_THROWN     equ 6
+ENTITY_STATE_STUNNED    equ 6
 ENTITY_STATE_LIFTED     equ 7
+ENTITY_STATE_THROWN     equ 8
 
 ; Values for wEntitiesTypeTable
 ENTITY_GOLDEN_LEAF       equ $30

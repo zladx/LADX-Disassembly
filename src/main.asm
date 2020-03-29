66ab2ad266eaf9ac9a53b900ff32dbdf50f38a9a
Pierre de La Morinerie <kemenaran@gmail.com>
2020-03-30 01:29:29 +0200

    entities: rename handlers file

--- src/main.asm
+++ src/main.asm
@@ -215,7 +215,7 @@ include "data/sfx/pointers_table.asm"
 include "code/audio/sfx.asm"
 
 section "bank20",romx[$4000],bank[$20]
-include "code/entities/_handlers_table.asm"
+include "code/entities/_handlers.asm"
 include "code/bank20.asm"
 
 section "bank21",romx[$4000],bank[$21]

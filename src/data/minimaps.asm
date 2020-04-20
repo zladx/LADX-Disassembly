07ba544b013f3f4ef87973c6561d1655d9413dfe
Xkeeper <xkeeper@gmail.com>
2020-04-19 22:51:50 -0700

    Use CHARMAPs to generate dungeon minimaps. Weird, but fancy?

--- src/data/minimaps.asm
+++ src/data/minimaps.asm
@@ -1,99 +1,122 @@
+; Dungeon minimaps, visible on the subscreen
+
+; The actual tile used for a visited room
+; is based off of the room data itself and the save file
+; using specific "doorway" objects in each room,
+; rather than being coded into the minimaps themselves
+
+; This is kind of a silly way to write them out in assembly
+; but hopefully a better visual metaphor than raw values.
+
+
+; Fun rgbds gotcha: NEWCHARMAP also switches to it.
+; This is not indicated in the manual. -___-
+PUSHC
+NEWCHARMAP MinimapCharmap
+CHARMAP "  ", $7D   ; Blank (not shown on map)
+CHARMAP "##", $EF   ; Room (shows up on map)
+CHARMAP "Ch", $ED   ; Room with chest
+CHARMAP "Nm", $EE   ; Nightmare boss marker
+
 Minimap0::
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $EF, $7D
-    db   $7D, $EF, $EF, $EF, $7D, $7D, $EE, $7D
-    db   $ED, $7D, $EF, $ED, $EF, $ED, $EF, $7D
-    db   $EF, $EF, $ED, $ED, $EF, $EF, $EF, $7D
-    db   $EF, $7D, $EF, $ED, $ED, $7D, $7D, $7D
-    db   $7D, $ED, $EF, $EF, $7D, $7D, $7D, $7D
+    db "                "
+    db "                "
+    db "            ##  "
+    db "  ######    Nm  "
+    db "Ch  ##Ch##Ch##  "
+    db "####ChCh######  "
+    db "##  ##ChCh      "
+    db "  Ch####        "
 
 Minimap1::
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $7D, $ED, $ED, $ED, $EF, $EF, $EF, $7D
-    db   $7D, $7D, $ED, $7D, $7D, $ED, $7D, $7D
-    db   $7D, $EF, $EF, $7D, $7D, $EF, $EE, $7D
-    db   $7D, $EF, $7D, $7D, $7D, $7D, $EF, $7D
-    db   $7D, $ED, $7D, $7D, $7D, $7D, $EF, $7D
-    db   $7D, $EF, $EF, $EF, $EF, $EF, $EF, $7D
-    db   $7D, $7D, $ED, $ED, $ED, $ED, $7D, $7D
+    db "                "
+    db "  ChChCh######  "
+    db "    Ch    Ch    "
+    db "  ####    ##Nm  "
+    db "  ##        ##  "
+    db "  Ch        ##  "
+    db "  ############  "
+    db "    ChChChCh    "
 
 Minimap2::
-    db   $EF, $EF, $ED, $EF, $7D, $7D, $7D, $7D
-    db   $ED, $EF, $ED, $ED, $7D, $7D, $7D, $7D
-    db   $EF, $EF, $EF, $EF, $7D, $7D, $EF, $7D
-    db   $7D, $ED, $EF, $7D, $7D, $EF, $EF, $EF
-    db   $7D, $ED, $7D, $7D, $7D, $7D, $EF, $7D
-    db   $7D, $ED, $ED, $7D, $7D, $7D, $EF, $7D
-    db   $7D, $ED, $7D, $7D, $7D, $7D, $EE, $7D
-    db   $7D, $EF, $ED, $7D, $7D, $7D, $EF, $EF
+    db "####Ch##        "
+    db "Ch##ChCh        "
+    db "########    ##  "
+    db "  Ch##    ######"
+    db "  Ch        ##  "
+    db "  ChCh      ##  "
+    db "  Ch        Nm  "
+    db "  ##Ch      ####"
 
 Minimap3::
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $7D, $7D, $7D, $ED, $EF, $7D, $7D, $7D
-    db   $7D, $EF, $7D, $EF, $EF, $7D, $ED, $7D
-    db   $7D, $EE, $EF, $ED, $EF, $ED, $EF, $7D
-    db   $7D, $EF, $ED, $ED, $EF, $EF, $ED, $7D
-    db   $7D, $7D, $EF, $EF, $EF, $ED, $7D, $7D
-    db   $7D, $7D, $ED, $EF, $ED, $ED, $7D, $7D
-    db   $7D, $7D, $7D, $EF, $ED, $7D, $7D, $7D
+    db "                "
+    db "      Ch##      "
+    db "  ##  ####  Ch  "
+    db "  Nm##Ch##Ch##  "
+    db "  ##ChCh####Ch  "
+    db "    ######Ch    "
+    db "    Ch##ChCh    "
+    db "      ##Ch      "
 
 Minimap4::
-    db   $7D, $EF, $EF, $EF, $ED, $EF, $7D, $7D
-    db   $7D, $7D, $7D, $EE, $7D, $EF, $ED, $7D
-    db   $7D, $EF, $EF, $EF, $EF, $EF, $ED, $ED
-    db   $7D, $7D, $7D, $7D, $7D, $EF, $EF, $EF
-    db   $7D, $7D, $7D, $EF, $EF, $EF, $EF, $7D
-    db   $7D, $7D, $ED, $EF, $EF, $EF, $7D, $7D
-    db   $7D, $7D, $7D, $ED, $EF, $EF, $7D, $7D
-    db   $7D, $7D, $7D, $7D, $ED, $EF, $ED, $EF
+    db "  ######Ch##    "
+    db "      Nm  ##Ch  "
+    db "  ##########ChCh"
+    db "          ######"
+    db "      ########  "
+    db "    Ch######    "
+    db "      Ch####    "
+    db "        Ch##Ch##"
 
 Minimap5::
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $ED, $7D, $7D, $7D, $7D, $7D, $7D, $ED
-    db   $EF, $ED, $7D, $EF, $EF, $7D, $ED, $EF
-    db   $EF, $ED, $EF, $EF, $EE, $EF, $ED, $EF
-    db   $ED, $EF, $7D, $EF, $EF, $7D, $EF, $EF
-    db   $7D, $EF, $ED, $EF, $EF, $EF, $EF, $7D
-    db   $7D, $ED, $ED, $7D, $7D, $EF, $ED, $7D
-    db   $7D, $EF, $EF, $EF, $EF, $EF, $EF, $7D
+    db "                "
+    db "Ch            Ch"
+    db "##Ch  ####  Ch##"
+    db "##Ch####Nm##Ch##"
+    db "Ch##  ####  ####"
+    db "  ##Ch########  "
+    db "  ChCh    ##Ch  "
+    db "  ############  "
 
 Minimap6::
-    db   $7D, $ED, $ED, $7D, $7D, $7D, $7D, $7D
-    db   $EF, $EF, $EF, $EF, $7D, $EF, $EF, $7D
-    db   $EF, $EF, $EF, $ED, $7D, $EF, $EE, $7D
-    db   $EF, $ED, $EF, $EF, $7D, $7D, $7D, $7D
-    db   $ED, $EF, $EF, $ED, $7D, $EF, $ED, $7D
-    db   $EF, $EF, $EF, $EF, $EF, $EF, $EF, $ED
-    db   $ED, $EF, $EF, $EF, $EF, $EF, $EF, $EF
-    db   $EF, $EF, $EF, $EF, $7D, $EF, $EF, $7D
+    db "  ChCh          "
+    db "########  ####  "
+    db "######Ch  ##Nm  "
+    db "##Ch####        "
+    db "Ch####Ch  ##Ch  "
+    db "##############Ch"
+    db "Ch##############"
+    db "########  ####  "
 
 Minimap7::
-    db   $7D, $7D, $7D, $EF, $EF, $7D, $7D, $7D
-    db   $ED, $7D, $7D, $EE, $ED, $7D, $7D, $ED
-    db   $EF, $EF, $ED, $EF, $EF, $ED, $EF, $EF
-    db   $7D, $ED, $EF, $EF, $EF, $EF, $EF, $7D
-    db   $7D, $ED, $EF, $EF, $EF, $EF, $EF, $7D
-    db   $EF, $ED, $EF, $ED, $EF, $EF, $EF, $EF
-    db   $EF, $ED, $EF, $EF, $EF, $ED, $EF, $EF
-    db   $ED, $7D, $7D, $EF, $EF, $7D, $7D, $ED
+    db "      ####      "
+    db "Ch    NmCh    Ch"
+    db "####Ch####Ch####"
+    db "  Ch##########  "
+    db "  Ch##########  "
+    db "##Ch##Ch########"
+    db "##Ch######Ch####"
+    db "Ch    ####    Ch"
 
 EaglesTowerCollapsedMinimap::
-    db   $7D, $ED, $ED, $7D, $7D, $7D, $7D, $7D
-    db   $EF, $EF, $EF, $EF, $7D, $7D, $7D, $7D
-    db   $EF, $EF, $EF, $ED, $7D, $7D, $7D, $7D
-    db   $EF, $ED, $EF, $EF, $7D, $7D, $7D, $7D
-    db   $ED, $EF, $EF, $ED, $7D, $EF, $ED, $7D
-    db   $EF, $EF, $EF, $EF, $EF, $EF, $EF, $EF
-    db   $ED, $EF, $EF, $EF, $EF, $EF, $EE, $EF
-    db   $EF, $EF, $EF, $EF, $7D, $EF, $EF, $7D
+    db "  ChCh          "
+    db "########        "
+    db "######Ch        "
+    db "##Ch####        "
+    db "Ch####Ch  ##Ch  "
+    db "################"
+    db "Ch##########Nm##"
+    db "########  ####  "
 
 ColorDungeonMinimap::
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $7D, $7D, $7D, $7D, $7D, $7D, $7D, $7D
-    db   $7D, $EE, $EF, $7D, $7D, $ED, $EF, $7D
-    db   $7D, $EF, $EF, $ED, $EF, $EF, $EF, $7D
-    db   $7D, $7D, $EF, $EF, $EF, $EF, $7D, $7D
-    db   $7D, $7D, $EF, $ED, $EF, $ED, $7D, $7D
-    db   $7D, $7D, $EF, $EF, $ED, $EF, $7D, $7D
+    db "                "
+    db "                "
+    db "                "
+    db "  Nm##    Ch##  "
+    db "  ####Ch######  "
+    db "    ########    "
+    db "    ##Ch##Ch    "
+    db "    ####Ch##    "
+
+
+POPC

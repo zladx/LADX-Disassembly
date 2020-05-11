443e1de66fdafce9a30ac29db68387dacfe2423d
Vextrove <52584544+Vextrove@users.noreply.github.com>
2020-05-11 17:07:51 +0200

    Labeled 75 of the tracks

--- src/constants/sfx.asm
+++ src/constants/sfx.asm
@@ -5,72 +5,74 @@
 ; their actual in-game use (???)
 
 ; Values for hMusicTrack
-MUSIC_NONE                                      equ $00
-MUSIC_TITLE_SCREEN                              equ $01
-MUSIC_TRENDY_GAME                               equ $02
-MUSIC_GAME_OVER                                 equ $03
-MUSIC_MABE_VILLAGE                              equ $04
-MUSIC_OVERWORLD                                 equ $05
-MUSIC_TAL_TAL_HEIGHTS                           equ $06
-MUSIC_VILLAGE_SHOP                              equ $07
-MUSIC_RAFT_RIDE_RAPIDS                          equ $08
-MUSIC_MYSTERIOUS_FOREST                         equ $09
-MUSIC_HOME_TRADER_HOUSE                         equ $0A
-MUSIC_ANIMAL_VILLAGE                            equ $0B
-MUSIC_FAIRY_HOUSE                               equ $0C
-MUSIC_TITLE                                     equ $0D
-MUSIC_BOWWOW_KIDNAPPED                          equ $0E
-MUSIC_FOUND_LEVEL_2_SWORD                       equ $0F
-MUSIC_FOUND_NEW_WEAPON                          equ $10
-MUSIC_2D_UNDERGROUND_DUNGEON                    equ $11
-MUSIC_OWL                                       equ $12
-MUSIC_FINAL_NIGHTMARE_IN_EGG                    equ $13
-MUSIC_DREAM_SHRINE_ENTRANCE                     equ $14
-MUSIC_FOUND_INSTRUMENT                          equ $15
-MUSIC_OVERWORLD_CAVE                            equ $16
-MUSIC_PIECE_OF_POWER                            equ $17
-MUSIC_RECEIVED_HORN_INSTRUMENT                  equ $18
-MUSIC_RECEIVED_BELL_INSTRUMENT                  equ $19
-MUSIC_RECEIVED_HARP_INSTRUMENT                  equ $1A
-MUSIC_RECEIVED_XYLOPHONE_INSTRUMENT             equ $1B
-;MUSIC_RECEIVED_UNKNOWN_INSTRUMENT              equ $1C
-;MUSIC_RECEIVED_UNKNOWN_INSTRUMENT              equ $1D
-MUSIC_RECEIVED_THUNDER_DRUM_INSTRUMENT          equ $1E
-MUSIC_MARIN_SINGING                             equ $1F
-MUSIC_MANBO_SONG                                equ $20
-;MUSIC_RECEIVED ?? INSTRUMENT                   equ $21
-;MUSIC_INSTRUMENTS_SONG ??                      equ $22
-;MUSIC_INSTRUMENTS_SONG ??                      equ $23
-MUSIC_DREAM_SHRINE_BED                          equ $24
-MUSIC_COMPLETE_INSTRUMENTS_SONG_PART_1          equ $25
-MUSIC_COMPLETE_INSTRUMENTS_SONG_PART_2          equ $26
-;MUSIC_INSTRUMENTS_SONG ??                      equ $27
-MUSIC_LONELY_HOUSE                              equ $28
-MUSIC_PIECE_OF_POWER_PART_2                     equ $29
-MUSIC_MARIN_SINGING_LINKS_OCARINA               equ $2A
-MUSIC_LEVEL_5                                   equ $2B
-MUSIC_DUNGEON_ENTRANCE_UNLOCKING                equ $2C
-MUSIC_DREAM_SEQUENCE_SOUND                      equ $2D
-MUSIC_AT_BEACH_WITH_MARIN                       equ $2E
-MUSIC_UNKNOWN                                   equ $2F
-MUSIC_DUNGEON_SUB_BOSS                          equ $30
-MUSIC_RECEIVED_LEVEL_1_SWORD                    equ $31
-MUSIC_MR_WRITE_HOUSE                            equ $32
-MUSIC_ULRIRA_HOUSE                              equ $33
-MUSIC_TARIN_ATTACKED_BY_BEES                    equ $34
-MUSIC_MAMU_SONG                                 equ $35
-MUSIC_MONKEYS_BUILDING_BRIDGE                   equ $36
-MUSIC_MR_WRITE_HOUSE_VERSION_2                  equ $37
-MUSIC_RICHARD_HOUSE_SECRET_SONG                 equ $38
-MUSIC_TURTLE_ROCK_ENTRANCE_BOSS                 equ $39
-MUSIC_FISHING_GAME                              equ $3A
-MUSIC_RECEIVED_ITEM                             equ $3B
-MUSIC_HIDDEN_UNUSED_SONG                        equ $3C
-MUSIC_NOTHING                                   equ $3D
-MUSIC_BOWWOW_STOLEN                             equ $3E
-MUSIC_ENDING                                    equ $3F
-MUSIC_RICHARD_S_HOUSE                           equ $40
-;MUSIC_GLITCHED_NOISE                           equ $41
+; Values for wActiveMusicTrack (d368)??
+                                                       ;No comment = correct.
+MUSIC_NONE                                      equ $00;
+MUSIC_TITLE_SCREEN                              equ $01;probably correct
+MUSIC_TRENDY_GAME                               equ $02;
+MUSIC_GAME_OVER                                 equ $03;
+MUSIC_MABE_VILLAGE                              equ $04;
+MUSIC_OVERWORLD                                 equ $05;
+MUSIC_TAL_TAL_HEIGHTS                           equ $06;
+MUSIC_VILLAGE_SHOP                              equ $07;	Witch hut.
+MUSIC_RAFT_RIDE_RAPIDS                          equ $08;don't remember where it is used; probably indeed raft minigame.
+MUSIC_MYSTERIOUS_FOREST                         equ $09;
+MUSIC_HOME_TRADER_HOUSE                         equ $0A;	Inside the houses.
+MUSIC_ANIMAL_VILLAGE                            equ $0B;
+MUSIC_FAIRY_HOUSE                               equ $0C;	Inside a Fairy Fountain
+MUSIC_TITLE                                     equ $0D;classic zelda theme, used when acorn/triangle is collected?
+MUSIC_BOWWOW_KIDNAPPED                          equ $0E;don't remember where it is used. intense theme, probably correct
+MUSIC_FOUND_LEVEL_2_SWORD                       equ $0F; Sword found. Not sure if specifically for the LVL2 sword.
+MUSIC_FOUND_NEW_WEAPON                          equ $10;item received jingle. when finished, reloads the current maps music
+MUSIC_2D_UNDERGROUND_DUNGEON                    equ $11;	File Select Screen.
+MUSIC_OWL                                       equ $12;don't know. Definitely not owl, probs a dungeon/cave.
+MUSIC_FINAL_NIGHTMARE_IN_EGG                    equ $13;unrecognized. Sounds dungeonlike
+MUSIC_DREAM_SHRINE_ENTRANCE                     equ $14;probably a dungeon theme
+MUSIC_FOUND_INSTRUMENT                          equ $15;dungeon theme.
+MUSIC_OVERWORLD_CAVE                            equ $16;don't know. Probably a dungeon
+MUSIC_PIECE_OF_POWER                            equ $17;Faster/higher version of common cave theme as heard at $26
+MUSIC_RECEIVED_HORN_INSTRUMENT                  equ $18;unknown item receive jingle
+MUSIC_RECEIVED_BELL_INSTRUMENT                  equ $19;boss battle theme, sounds different from what I'm used to(?)
+MUSIC_RECEIVED_HARP_INSTRUMENT                  equ $1A;	Intro cutscene.
+MUSIC_RECEIVED_XYLOPHONE_INSTRUMENT             equ $1B;item receive thing
+;MUSIC_RECEIVED_UNKNOWN_INSTRUMENT              equ $1C;alternative house theme? not sure
+;MUSIC_RECEIVED_UNKNOWN_INSTRUMENT              equ $1D;	First time on Koholint Island.
+MUSIC_RECEIVED_THUNDER_DRUM_INSTRUMENT          equ $1E;bed sleeping dream world entering cutscene?
+MUSIC_MARIN_SINGING                             equ $1F;sounds like a dungeon, but has marin's song. Probably cutscene related
+MUSIC_MANBO_SONG                                equ $20; Marin melodic motif on violin-like instrument.
+;MUSIC_RECEIVED ?? INSTRUMENT                   equ $21;	2D sidescrolling cave. (where the Goombas are)
+;MUSIC_INSTRUMENTS_SONG ??                      equ $22;	Owl theme.
+;MUSIC_INSTRUMENTS_SONG ??                      equ $23;	Final Boss theme. (shadow face with 2 arms)
+MUSIC_DREAM_SHRINE_BED                          equ $24;	Dream Shrine.
+MUSIC_COMPLETE_INSTRUMENTS_SONG_PART_1          equ $25;Item jingle
+MUSIC_COMPLETE_INSTRUMENTS_SONG_PART_2          equ $26;Common cave passageway theme. $17 is a variation on this theme.
+;MUSIC_INSTRUMENTS_SONG ??                      equ $27;	Super acorn and/or power piece jingle+theme. Like Starman in Mario.
+MUSIC_LONELY_HOUSE                              equ $28; Marin melodic motif on horn-like instrument.
+MUSIC_PIECE_OF_POWER_PART_2                     equ $29;Sounds like annoying mobile ringtone. (Item?) jingle.
+MUSIC_MARIN_SINGING_LINKS_OCARINA               equ $2A;unknown. Incorrectly labeled.
+MUSIC_LEVEL_5                                   equ $2B;unknown jingle. 
+MUSIC_DUNGEON_ENTRANCE_UNLOCKING                equ $2C; Theme played on a triangle. Only uses one note in a rhythm.
+MUSIC_DREAM_SEQUENCE_SOUND                      equ $2D; Marin melodic motif on a flute-like instrument.
+MUSIC_AT_BEACH_WITH_MARIN                       equ $2E;Funky bongo
+MUSIC_UNKNOWN                                   equ $2F;	Marin's singing the Ballad of the Wind Fish. Beautiful theme.
+MUSIC_DUNGEON_SUB_BOSS                          equ $30;	Manbo's Mambo
+MUSIC_RECEIVED_LEVEL_1_SWORD                    equ $31; Overworld theme, with introduction section.
+MUSIC_MR_WRITE_HOUSE                            equ $32;
+MUSIC_ULRIRA_HOUSE                              equ $33;	Phone Booth.
+MUSIC_TARIN_ATTACKED_BY_BEES                    equ $34; 
+MUSIC_MAMU_SONG                                 equ $35;	Mamu's/Wart's song.
+MUSIC_MONKEYS_BUILDING_BRIDGE                   equ $36;
+MUSIC_MR_WRITE_HOUSE_VERSION_2                  equ $37;	
+MUSIC_RICHARD_HOUSE_SECRET_SONG                 equ $38;	Totaka's Song, different instrument from the one in Richards house.
+MUSIC_TURTLE_ROCK_ENTRANCE_BOSS                 equ $39;Rumbling noise, cutscene, then a boss theme.
+MUSIC_FISHING_GAME                              equ $3A;	Mashup of Witch Hut and Trendy Game 
+MUSIC_RECEIVED_ITEM                             equ $3B; Short item jingle
+MUSIC_HIDDEN_UNUSED_SONG                        equ $3C;	Introduction like ZELDA easter egg, then Totaka's Song. 
+MUSIC_NOTHING                                   equ $3D; Silence, then marin's theme. Probably from a cutscene.
+MUSIC_BOWWOW_STOLEN                             equ $3E;Chromatic introduction, then tense loop.
+MUSIC_ENDING                                    equ $3F; Marin's motif on instruments. Epic mashup.
+MUSIC_RICHARD_S_HOUSE                           equ $40; Needs renaming for consistency e.g. RICHARDS_HOUSE or RICHARD_HOUSE
+;MUSIC_GLITCHED_NOISE                           equ $41; Marin's motif on horn and flute-like instruments.
 ;MUSIC_GLITCHED_NOISE                           equ $42
 ;MUSIC_GLITCHED_NOISE                           equ $43
 ;MUSIC_GLITCHY                                  equ $44

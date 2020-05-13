e0f0981220e5656331a25ad74c7c58effccaf56b
Vextrove <52584544+Vextrove@users.noreply.github.com>
2020-05-13 15:48:20 +0200

    wActiveMusicTrack -> wPlayMusicTrack

--- src/constants/sfx.asm
+++ src/constants/sfx.asm
@@ -1,24 +1,24 @@
 ;
 ; Constants for audio effects
 
-; Values for wActiveMusicTrack
+; Values for wPlayMusicTrack
  
-MUSIC_NONE                                      equ $00
-MUSIC_TITLE_SCREEN_INTRODUCTION                 equ $01; The title screen's theme's introduction section.
+MUSIC_NONE                                      equ $00 ; This value means: Don't play a different track.
+MUSIC_TITLE_SCREEN_INTRODUCTION                 equ $01	; $0D's introduction section.
 MUSIC_TRENDY_GAME                               equ $02
 MUSIC_GAME_OVER                                 equ $03
 MUSIC_MABE_VILLAGE                              equ $04
 MUSIC_OVERWORLD                                 equ $05
-MUSIC_MT_TAMARANCH                              equ $06; Also Tal Tal Height.
-MUSIC_WITCH_HUT                                 equ $07; Also used in the shop.
+MUSIC_MT_TAMARANCH                              equ $06	; Also Tal Tal Height.
+MUSIC_WITCH_HUT                                 equ $07	; Also used in the shop.
 MUSIC_RAFT_RIDE_RAPIDS                          equ $08
 MUSIC_MYSTERIOUS_FOREST                         equ $09
-MUSIC_HOUSE                                     equ $0A; Inside most regular houses.
+MUSIC_HOUSE                                     equ $0A	; Inside most regular houses.
 MUSIC_ANIMAL_VILLAGE                            equ $0B
 MUSIC_FAIRY_FOUNTAIN                            equ $0C
-MUSIC_TITLE_SCREEN                              equ $0D; Plays when $01 has finished.
-MUSIC_BOWWOW_KIDNAPPED                          equ $0E; Plays when $3E has finished.
-MUSIC_SWORD_ACQUIRED                            equ $0F; Possibly also for LVL2 sword
+MUSIC_TITLE_SCREEN                              equ $0D	; Plays when $01 has finished.
+MUSIC_BOWWOW_KIDNAPPED                          equ $0E	; Plays when $3E has finished.
+MUSIC_SWORD_ACQUIRED                            equ $0F	; Possibly also for LVL2 sword.
 MUSIC_WEAPON_ACQUIRED                           equ $10
 MUSIC_FILE_SELECT                               equ $11
 MUSIC_INSIDE_THE_WIND_EGG                       equ $12
@@ -27,12 +27,12 @@ MUSIC_TAIL_CAVE                                 equ $14
 MUSIC_BOTTLE_GROTTO                             equ $15
 MUSIC_KEY_CAVERN                                equ $16
 MUSIC_ANGLERS_TUNNEL                            equ $17
-MUSIC_BOSS_DEFEATED                             equ $18; Plays in boss rooms after the boss has been defeated.
+MUSIC_BOSS_DEFEATED                             equ $18	; Plays in boss rooms after the boss has been defeated.
 MUSIC_BOSS_BATTLE                               equ $19
 MUSIC_INTRO_CUTSCENE                            equ $1A
 MUSIC_INSTRUMENT_ACQUIRED                       equ $1B
-MUSIC_LINK_AWAKENS                              equ $1C; Played in the house when you begin your game.
-MUSIC_SWORD_SEARCH                              equ $1D; First time on Koholint Island.
+MUSIC_LINK_AWAKENS                              equ $1C	; Played in the house when you begin your game.
+MUSIC_SWORD_SEARCH                              equ $1D	; First time on Koholint Island.
 MUSIC_DREAMING                                  equ $1E
 MUSIC_SOUTHERN_SHRINE                           equ $1F 
 MUSIC_FULL_MOON_CELLO                           equ $20
@@ -52,74 +52,77 @@ MUSIC_INSTRUMENT_ORGAN_OF_EVENING               equ $2D
 MUSIC_THUNDER_DRUM                              equ $2E
 MUSIC_MARIN_SINGING                             equ $2F
 MUSIC_MANBOS_MAMBO                              equ $30
-MUSIC_OVERWORLD_INTRODUCTION                    equ $31;    The overworld theme's introduction section.
+MUSIC_OVERWORLD_INTRODUCTION                    equ $31	; $05's introduction section after retrieving your sword.
 MUSIC_MR_WRITE_HOUSE                            equ $32
-MUSIC_PHONE_BOOTH                               equ $33; Also Ulrira's house.
+MUSIC_PHONE_BOOTH                               equ $33	; Also Ulrira's house.
 MUSIC_TARIN_ATTACKED_BY_BEES                    equ $34
 MUSIC_MAMUS_SONG                                equ $35
 MUSIC_MONKEYS_BUILDING_BRIDGE                   equ $36
-;MUSIC_MR_WRITE_HOUSE_VERSION_2                 equ $37; Mr Write House variant. Where is this used?
-MUSIC_TOTAKAS_SONG                              equ $38; Totaka's Song as played in Richard's mansion; does not init instrument bank. Missing drums(?)
+;MUSIC_MR_WRITE_HOUSE_VERSION_2                 equ $37	; mr write house variant. where is this used?
+MUSIC_TOTAKAS_SONG                              equ $38	; Totaka's Song as played in Richard's mansion	; does not init instrument bank. Missing drums(?)
 MUSIC_TURTLE_ROCK_ENTRANCE_BOSS                 equ $39
 MUSIC_FISHING_GAME                              equ $3A
-;MUSIC_RECEIVED_ITEM                            equ $3B; Short item jingle
-MUSIC_TOTAKEKE_NICKNAME_EASTER_EGG              equ $3C; とたけけ nickname easter egg. Unused in EU/US versions. Similar to $60
-MUSIC_ENDING                                    equ $3D; During ending cutscene. Silence, then music. Plays staff roll theme when finished.
+;MUSIC_RECEIVED_ITEM                            equ $3B	; short item jingle
+MUSIC_TOTAKEKE_NICKNAME_EASTER_EGG              equ $3C	; とたけけ nickname easter egg. Unused in EU/US versions. Similar to $60
+MUSIC_ENDING                                    equ $3D	; During ending cutscene. Silence, then music. Plays staff roll theme when finished.
 MUSIC_BOWWOW_KIDNAPPED_INTRODUCTION             equ $3E
-MUSIC_WIND_FISH_AWAKENS                         equ $3F; Plays during cutscene after defeating the final boss
+MUSIC_WIND_FISH_AWAKENS                         equ $3F	; Plays during cutscene after defeating the final boss
 MUSIC_RICHARDS_HOUSE                            equ $40
-;MUSIC_BOTWF                                    equ $41; Ballad of the Wind Fish, on horn and flute-like instruments.
-;MUSIC_BOTWF                                    equ $42; Ballad of the Wind Fish, has added marimba motif. More voices than $41
-;MUSIC_BOTWF                                    equ $43; Ballad of the Wind Fish, piano, more voices than $42
-;MUSIC_BOTWF                                    equ $44; Ballad of the Wind Fish, has high pitched ting. more voices than $43
-;MUSIC_BOTWF                                    equ $45; Ballad of the Wind Fish, has triangle rhythm. More voices than $44
-;MUSIC_BOTWF                                    equ $46; Ballad of the Wind Fish, has added horn. More voices than $45
-;MUSIC_BOTWF                                    equ $47; Ballad of the Wind Fish, drums, high ting. Awakening tracks are not well described.
+;MUSIC_BOTWF                                    equ $41	; ballad of the wind fish, on horn and flute-like instruments.
+;MUSIC_BOTWF                                    equ $42	; ballad of the wind fish, has added marimba motif. more voices than $41
+;MUSIC_BOTWF                                    equ $43	; ballad of the wind fish, piano, more voices than $42
+;MUSIC_BOTWF                                    equ $44	; ballad of the wind fish, has high pitched ting. more voices than $43
+;MUSIC_BOTWF                                    equ $45	; ballad of the wind fish, has triangle rhythm. more voices than $44
+;MUSIC_BOTWF                                    equ $46	; ballad of the wind fish, has added horn. more voices than $45
+;MUSIC_BOTWF                                    equ $47	; ballad of the wind fish, drums, high ting. awakening tracks are not well described.
 MUSIC_GHOSTS_HOUSE                              equ $48
 MUSIC_ACTIVE_POWER_UP                           equ $49
-;MUSIC_SILENCE                                  equ $4A; More awakening...
+;MUSIC_SILENCE                                  equ $4A	; more awakening...
 MUSIC_CATFISHS_MAW                              equ $4B
-;MUSIC_GLITCHY                                  equ $4C; Orchestral, short jingle.
-;MUSIC_SILENCE                                  equ $4D; harp jingle
+;MUSIC_UNKNOWN                                  equ $4C	; orchestral, short jingle.
+;MUSIC_UNKNOWN                                  equ $4D	; harp jingle
 MUSIC_MARIN_BEACH                               equ $4E
-;MUSIC_UNKNOWN                                  equ $4F; item jingle
+;MUSIC_UNKNOWN                                  equ $4F	; item jingle
 MUSIC_MINI_BOSS                                 equ $50
-MUSIC_KANALET_CASTLE_COPY                       equ $51; Copy of $13.
-MUSIC_TAIL_CAVE_COPY                            equ $52; Copy of $14.
+MUSIC_KANALET_CASTLE_COPY                       equ $51	; Copy of $13.
+MUSIC_TAIL_CAVE_COPY                            equ $52	; Copy of $14.
 MUSIC_DREAM_SHRINE                              equ $53
-;MUSIC_UNKNOWN                                  equ $54; intense arpeggio, pre boss music? Switches to $19.
+;MUSIC_UNKNOWN                                  equ $54	; intense arpeggio, pre boss music? Switches to $19.
 MUSIC_FLYING_ROOSTER_REVIVED                    equ $55
-;MUSIC_UNKNOWN                                  equ $56; punch noises in upwards chord progression, plays $55 afterwards
-;MUSIC_CUCCO_HOUSE                              equ $57; Is called 'Cucco House'
+;MUSIC_UNKNOWN                                  equ $56	; punch noises in upwards chord progression, plays $55 afterwards
+MUSIC_CUCCO_HOUSE                               equ $57
 MUSIC_FACE_SHRINE                               equ $58
-;MUSIC_WINDFISH                                 equ $59; Is called 'The WindFish'
+;MUSIC_WINDFISH                                 equ $59	; Is called 'The WindFish'
 MUSIC_TURTLE_ROCK                               equ $5A
 MUSIC_EAGLES_TOWER                              equ $5B
-MUSIC_BEFORE_EAGLES_TOWER_BOSS                  equ $5C; Plays before the boss at the top of Eagle's Tower is summon by Grim Creeper.
-MUSIC_FINAL_BOSS_DIALOG                         equ $5D; Plays during the final boss' dialog.
-MUSIC_BOSS_WARNING                              equ $5E; Plays during the dialogs where the bosses tell you not to wake the Wind Fish.
+MUSIC_BEFORE_EAGLES_TOWER_BOSS                  equ $5C	; Plays before the boss at the top of Eagle's Tower is summon by Grim Creeper.
+MUSIC_FINAL_BOSS_DIALOG                         equ $5D	; Plays during the final boss' dialog.
+MUSIC_BOSS_WARNING                              equ $5E	; Plays during the dialogs where the bosses tell you not to wake the Wind Fish.
 MUSIC_FINAL_BOSS_DEFEATED                       equ $5F
-MUSIC_ZELDA_NICKNAME_EASTER_EGG                 equ $60; ZELDA nickname easter egg.
-MUSIC_COLOR_DUNGEON                             equ $61;
-;MUSIC_COLOR_DUNGEON_COPIES                     equ $62 to 6F;	Same as $61	
-;MUSIC_GLITCHED                                 equ $70 to $FE;	Glitches the music that played before it.
-MUSIC_NOTHING                                   equ $FF;	Silence.
+MUSIC_ZELDA_NICKNAME_EASTER_EGG                 equ $60	; ZELDA nickname easter egg.
+MUSIC_COLOR_DUNGEON                             equ $61
+;MUSIC_COLOR_DUNGEON_COPIES                     equ $62 to 6F	; Same as $61	
+;MUSIC_GLITCHED                                 equ $70 to $FE	; Glitches the music that played before it.
+MUSIC_SILENCE_VOICE_CHANNELS                    equ $FF	; Changes the voice channels. Does not always silence currently playing tracks. 
 
 ; Values for hJingle
-JINGLE_TREASURE_FOUND   equ $01
-JINGLE_PUZZLE_SOLVED    equ $02
-JINGLE_BOW_WOW_CHOMP    equ $03
-JINGLE_CHARGING_SWORD   equ $04
-JINGLE_POWDER           equ $05
-JINGlE_ENNEMY_MORPH_IN  equ $06
+JINGLE_NONE             equ $00;
+JINGLE_TREASURE_FOUND   equ $01;treasure
+JINGLE_PUZZLE_SOLVED    equ $02;
+JINGLE_BOW_WOW_CHOMP    equ $03;damage
+JINGLE_CHARGING_SWORD   equ $04;
+JINGLE_POWDER           equ $05;powder
+JINGlE_ENNEMY_MORPH_IN  equ $06;mario mushroom
 JINGLE_SWORD_POKING     equ $07
 JINGLE_JUMP_DOWN        equ $08
 JINGLE_BUMP             equ $09
 JINGLE_MOVE_SELECTION   equ $0A
-JINGLE_HUGE_BUMP        equ $0B
+JINGLE_HUGE_BUMP        equ $0B;huge bump
 JINGLE_REVOLVING_DOOR   equ $0C
 JINGLE_FEATHER_JUMP     equ $0D
 JINGLE_WATER_DIVE       equ $0E
+;JINGLE_UNKNOWN         equ $0F
+;JINGLE_UNKNOWN         equ $10
 JINGLE_OPEN_INVENTORY   equ $11
 JINGLE_CLOSE_INVENTORY  equ $12
 JINGLE_VALIDATE         equ $13
@@ -129,6 +132,9 @@ JINGLE_SHIELD_TING      equ $16
 JINGLE_GOT_POWER_UP     equ $17
 JINGLE_ITEM_FALLING     equ $18
 JINGLE_NEW_HEART        equ $19
+;JINGLE_UNKNOWN         equ $1A
+;JINGLE_UNKNOWN         equ $1B
+;JINGLE_UNKNOWN         equ $1C
 JINGLE_WRONG_ANSWER     equ $1D
 JINGLE_FOREST_LOST      equ $1E
 JINGLE_ENNEMY_MORPH_OUT equ $1F
@@ -136,12 +142,33 @@ JINGLE_BIG_BUMP         equ $20
 JINGLE_SEAGULL          equ $21
 JINGLE_DUNGEON_OPENED   equ $23
 JINGLE_JUMP             equ $24
+;JINGLE_UNKNOWN         equ $25
+;JINGLE_UNKNOWN         equ $26
+;JINGLE_UNKNOWN         equ $27
+;JINGLE_UNKNOWN         equ $28
+;JINGLE_UNKNOWN         equ $29
+;JINGLE_UNKNOWN         equ $2A
 JINGLE_INSTRUMENT_WARP  equ $2B
 JINGLE_MANBO_WARP       equ $2C
 JINGLE_GHOST_PRESENCE   equ $2D
+;JINGLE_UNKNOWN         equ $2E
+;JINGLE_UNKNOWN         equ $2F
+;JINGLE_UNKNOWN         equ $30
+;JINGLE_UNKNOWN         equ $31
+;JINGLE_UNKNOWN         equ $32
+;JINGLE_UNKNOWN         equ $33
+;JINGLE_UNKNOWN         equ $34
+;JINGLE_UNKNOWN         equ $35
+;JINGLE_UNKNOWN         equ $36
+;JINGLE_UNKNOWN         equ $37
+;JINGLE_UNKNOWN         equ $38
+;JINGLE_UNKNOWN         equ $39
+;JINGLE_UNKNOWN         equ $3A
+;JINGLE_UNKNOWN         equ $2B
 JINGLE_ENNEMY_SHRIEK    equ $3C
 
 ; Values for hWaveSfx
+WAVE_SFX_NONE            equ $00
 WAVE_SFX_SEASHELL        equ $01
 WAVE_SFX_ZIP             equ $02
 WAVE_SFX_LINK_HURT       equ $03
@@ -154,39 +181,64 @@ WAVE_SFX_OCARINA_BALLAD  equ $09
 WAVE_SFX_OCARINA_FROG    equ $0A
 WAVE_SFX_OCARINA_MAMBO   equ $0B
 WAVE_SFX_LINK_FALLS      equ $0C
+;WAVE_SFX_UNKNOWN        equ $0D
+;WAVE_SFX_UNKNOWN        equ $0E
 WAVE_SFX_TYPEWRITER      equ $0F
 WAVE_SFX_BOSS_AGONY      equ $10
+;WAVE_SFX_UNKNOWN        equ $11
+;WAVE_SFX_UNKNOWN        equ $12
 WAVE_SFX_CUCCO_HURT      equ $13
+;WAVE_SFX_UNKNOWN        equ $14
 WAVE_SFX_OCARINA_NOSONG  equ $15	; Off-key notes for when you have no songs available
 WAVE_SFX_BOSS_HIT        equ $16
+;WAVE_SFX_UNKNOWN        equ $17
 WAVE_SFX_CHAIN_CHOMP     equ $18
 WAVE_SFX_HOOT            equ $19
+;WAVE_SFX_UNKNOWN        equ $1A
 WAVE_SFX_COMPASS         equ $1B
 
-; Values for hNoiseSfx
-NOISE_SFX_SWORD_A         equ $02
-NOISE_SFX_SPIN_ATTACK     equ $03
-NOISE_SFX_DOOR_UNLOCKED   equ $04
-NOISE_SFX_RUPEE           equ $05
-NOISE_SFX_STAIRS          equ $06
-NOISE_SFX_FOOTSTEP        equ $07
-NOISE_SFX_POT_SMASHED     equ $09
-NOISE_SFX_SHOOT_ARROW     equ $0A
-NOISE_SFX_BOMB_EXPLOSION  equ $0C
-NOISE_SFX_MAGIC_ROD       equ $0D
-NOISE_SFX_SHOWEL_DIG      equ $0E
-NOISE_SFX_SEA_WAVES       equ $0F
-NOISE_SFX_DOOR_CLOSED     equ $10
-NOISE_SFX_BLOCK_RUMBLE    equ $11
-NOISE_SFX_BURSTING_FLAME  equ $12
-NOISE_SFX_ENEMY_DESTROYED equ $13
-NOISE_SFX_SWORD_B         equ $14
-NOISE_SFX_SWORD_C         equ $15
-NOISE_SFX_DRAW_SHIELD     equ $16
-NOISE_SFX_SWORD_D         equ $18
-NOISE_SFX_TITLE_APPEARS   equ $19
-NOISE_SFX_DOOR_RUMBLE     equ $2A
-NOISE_SFX_ROCK_RUMBLE     equ $2B
-NOISE_SFX_BOOMERANG       equ $2D
-NOISE_SFX_EAGLE_TOUCHDOWN equ $30
-NOISE_SFX_EAGLE_LIFT_UP   equ $31
+; Values for hNoiseSfx and wActiveNoiseSfx
+NOISE_SFX_NONE                    equ $00;
+;NOISE_SFX_UNKNOWN                equ $01; Singular door sound, stairway?
+NOISE_SFX_SWORD_A                 equ $02;
+NOISE_SFX_SPIN_ATTACK             equ $03;
+NOISE_SFX_DOOR_UNLOCKED           equ $04;opening of door likely
+NOISE_SFX_CUT_GRASS               equ $05;
+NOISE_SFX_STAIRS                  equ $06;	Also used for doorways.
+NOISE_SFX_FOOTSTEP                equ $07;	Landing after jump
+NOISE_SFX_POT_SMASHED             equ $09;hit/stun, perhaps pot
+NOISE_SFX_SHOOT_ARROW             equ $0A;probs arrow
+;NOISE_SFX_SHIELD_DEFLECT         equ $0B;shield sound but shorter
+NOISE_SFX_BOMB_EXPLOSION          equ $0C;boom.
+NOISE_SFX_MAGIC_ROD               equ $0D;electricity, but likely magic
+NOISE_SFX_SHOWEL_DIG              equ $0E;digging
+NOISE_SFX_SEA_WAVES               equ $0F;beach waves
+NOISE_SFX_DOOR_CLOSED             equ $10;closing of door likely
+NOISE_SFX_BLOCK_RUMBLE            equ $11;some rumble
+NOISE_SFX_BURSTING_FLAME          equ $12;sounds like flame
+NOISE_SFX_ENEMY_DESTROYED         equ $13;
+NOISE_SFX_SWORD_B                 equ $14;
+NOISE_SFX_SWORD_C                 equ $15;
+NOISE_SFX_DRAW_SHIELD             equ $16;
+;NOISE_SFX_UNKNOWN                equ $17;LVL2 shield?
+NOISE_SFX_SWORD_D                 equ $18;
+NOISE_SFX_TITLE_APPEARS           equ $19;probably correct
+;NOISE_SFX_ELECTRICITY            equ $20;Electricity/Shopkeeper's magic 
+;NOISE_SFX_UNKNOWN                equ $21;silent?
+;NOISE_SFX_UNKNOWN                equ $22;beach/explosion with fade out
+;NOISE_SFX_UNKNOWN                equ $23;burning/explosion/rumbling
+;NOISE_SFX_UNKNOWN                equ $24;beach/fire doused
+;NOISE_SFX_UNKNOWN                equ $25;fire, definitely cutscene because timed hiccups
+;NOISE_SFX_UNKNOWN                equ $26;more electricity
+;NOISE_SFX_UNKNOWN                equ $27;breaking/fire/explosion
+;NOISE_SFX_UNKNOWN                equ $28;stunned enemy?
+;NOISE_SFX_UNKNOWN                equ $29;brick blocks break
+NOISE_SFX_DOOR_RUMBLE             equ $2A;probably entrance opening
+NOISE_SFX_ROCK_RUMBLE             equ $2B;ancient door sliding
+;NOISE_SFX_UNKNOWN                equ $2C;fade out, intro?
+NOISE_SFX_BOOMERANG               equ $2D;probably boomerang
+;NOISE_SFX_UNKNOWN                equ $2E;burning... then small door opening
+;NOISE_SFX_UNKNOWN                equ $2F;page of a book
+NOISE_SFX_EAGLE_TOUCHDOWN         equ $30;probably eagle wings
+NOISE_SFX_EAGLE_LIFT_UP           equ $31;also eagle probably
+; entries to be added to at least $AA

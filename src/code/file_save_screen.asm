e90dee734abb0efe63e0439159722c58fba228ba
Pierre de La Morinerie <kemenaran@gmail.com>
2020-05-28 00:12:30 +0200

    bank1: move some code to file_save_screen.asm

--- src/code/file_save_screen.asm
+++ src/code/file_save_screen.asm
@@ -127,7 +127,7 @@ FileSaveInteractive::
     call func_001_412A
     ldh  a, [hJoypadState]
     and  $B0
-    jr   z, LCDOn_return
+    jr   z, LCDOn.return
     ld   a, JINGLE_VALIDATE
     ldh  [hJingle], a
     ld   a, [$C13F]
@@ -144,4 +144,195 @@ FileSaveInteractive::
     ld   [$C50A], a
     ld   [$C116], a
 .done
+    ret
+
+; Unused code?
+label_40D6::
+    xor  a
+    ld   [wOBJ0Palette], a
+    ld   [wOBJ1Palette], a
+    ld   [rOBP0], a
+    ld   [rOBP1], a
+    ld   [wBGPalette], a
+    ld   [rBGP], a
+    ldh  a, [hLinkPositionX]
+    ld   [wMapEntrancePositionX], a
+    ldh  a, [hLinkPositionY]
+    ld   [wMapEntrancePositionY], a
+    call LoadSavedFile
+    ld   a, $80
+    ld   [wInvincibilityCounter], a
+    ret
+
+jr_001_40F9::
+    call label_27F2
+    call func_001_5DE6
+    call ClearWRAMAndLowerHRAM
+    xor  a
+    ldh  [hActiveEntityTilesOffset], a
+    ld   a, $01
+    ld   [$DBAF], a
+    call func_001_6162
+    ; fallthrough
+
+; Enable LCD Screen
+LCDOn::
+    ; Configure LCD control register
+    ;   Bit 7: Enable LDC display
+    ;   Bit 6: Window tile map to $9C00-$9FFF
+    ;   Bit 5: Window display disabled
+    ;   Bit 4: Background & Window Tile Data to $8800-$97FF
+    ;   Bit 3: Background Tile Map to $9800-$9BFF
+    ;   Bit 2: Sprite size 8x16
+    ;   Bit 1: Sprite displayed enabled
+    ;   Bit 0: Background display enabled
+    ld   a, %11000111
+    ld   [rLCDC], a
+    ld   [wLCDControl], a
+
+    ; Set Window X position
+    ld   a, $07
+    ld   [rWX], a
+
+    ; Set Windows Y position
+    ld   a, $80
+    ld   [wWindowY], a
+    ld   [rWY], a
+
+    ; Configure Window
+    ld   a, $07
+    ldh  [hWindowYUnused], a
+    ld   a, $70
+    ldh  [hWindowXUnused], a
+.return
+    ret
+
+Data_001_4128::
+    ld   c, b
+    ld   e, b
+
+; Called by FileSaveInteractive
+func_001_412A::
+    ld   hl, $C13F
+    call func_001_6BA8
+    ldh  a, [hJoypadState]
+    and  $0C
+    jr   z, jr_001_413B
+    ld   a, [hl]
+    inc  a
+    and  $01
+    ld   [hl], a
+
+jr_001_413B::
+    ld   e, [hl]
+    ld   d, $00
+    ld   hl, Data_001_4128
+    add  hl, de
+    ld   a, [hl]
+    ld   hl, $C018
+    ldi  [hl], a
+    ld   a, $24
+    ldi  [hl], a
+    ld   a, $BE
+    ldi  [hl], a
+    ld   [hl], $00
+    ret
+    ldh  a, [$FFB7]
+    and  a
+    jp   nz, .cleanup
+    ld   e, $70
+
+.wait1
+    xor  a
+    ld   [rBGP], a
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    dec  e
+    jr   nz, .wait1
+    ld   e, $30
+
+.loop1
+    ld   a, $40
+    ld   [rBGP], a
+    dec  e
+    jr   nz, .loop1
+    ld   e, $30
+
+.loop2
+    ld   a, $80
+    ld   [rBGP], a
+    dec  e
+    jr   nz, .loop2
+    ld   e, $FF
+
+.wait2
+    ld   a, $C0
+    ld   [rBGP], a
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    dec  e
+    jr   nz, .wait2
+    ld   e, $30
+
+.loop3
+    ld   a, $80
+    ld   [rBGP], a
+    dec  e
+    jr   nz, .loop3
+    ld   e, $30
+
+.loop4
+    ld   a, $40
+    ld   [rBGP], a
+    dec  e
+    jr   nz, .loop4
+
+.cleanup
+    xor  a
+    ld   [wBGPalette], a
+    ld   [rBGP], a
     ret

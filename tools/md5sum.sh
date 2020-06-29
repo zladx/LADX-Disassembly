dd0457bcaa07eccb8ae5139dccf4f6126a5a165f
Pierre de La Morinerie <kemenaran@gmail.com>
2020-06-29 10:41:22 +0200

    tools: always use a bash implementation of md5sum
    This allows us to ensure that md5sum always support the
    `--ignore-missing` option (older GNU md5sum don't).

--- tools/md5sum.sh
+++ tools/md5sum.sh
@@ -1,45 +1,52 @@
-#!/bin/bash
+#!/bin/bash -eu
 #
-# A wrapper of md5sum, for systems where the `md5sum` command may not be available (i.e. macOS)
+# A partial implementation of md5sum, for systems where the `md5sum` command
+# may not be available (i.e. macOS).
+#
 # In this case, this script provides the same functionnality, using the `md5` command
 # (if available).
 #
 # Usage:
-#   md5sum.sh -c ladx.md5
+#   md5sum.sh --check [--ignore-missing] ladx.md5
 
-OPTIONS="$1"
-SUMFILE="$2"
-
-HAS_MD5SUM=$(command -v md5sum)
-HAS_MD5=$(command -v md5)
-
-# Implementation based on md5sum
-_md5sum()
-{
-  md5sum -c "$SUMFILE"
-}
-
-# Implementation based on md5
-_md5()
-{
-  while read -r line ; do
-    FILE=$(echo "$line" | cut -d ' ' -f2)
-    if [[ $(md5 -r "$FILE") == "$line" ]]; then
-      echo "$FILE checksum: OK"
-    else
-      echo "$FILE checksum: FAILED"
-      exit 1
-    fi
-  done < "$SUMFILE"
-}
-
-# Execute using an available implementation
-if [[ $HAS_MD5SUM ]]; then
-  _md5sum
-elif [[ $HAS_MD5 ]]; then
-  _md5
+# Find an available implementation
+if [[ $(command -v md5sum) ]]; then
+  MD5="md5sum"
+elif [[ $(command -v md5) ]]; then
+  MD5="md5 -r"
 else
   echo 'md5sum.sh: found neither `md5sum` nor `md5` commands.'
   echo 'Please install either `md5sum` or `md5` on your system.'
   exit 1
 fi
+
+# Compare with the sumfile manually
+# (Because not all implementations of `md5sum` support
+# the `--ignore-missing` option.)
+
+# Parse options
+IGNORE_MISSING=0;
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+      -c|--check) ;;
+      --ignore-missing) IGNORE_MISSING=1 ;;
+      *) SUMFILE="$1" ;;
+  esac
+  shift
+done
+
+# For each line in the sumfile, compare the checksum with the expected value
+while read -r LINE ; do
+  FILE=$(echo "$LINE" | cut -d ' ' -f2)
+  if [[ $IGNORE_MISSING && ! -f "$FILE" ]]; then continue; fi
+
+  EXPECTED="$LINE"
+  ACTUAL=$($MD5 "$FILE" | tr -s ' ')
+  if [[ "$EXPECTED" == "$ACTUAL" ]]; then
+    echo "$FILE checksum: OK"
+  else
+    echo "$FILE checksum: FAILED"
+    exit 1
+  fi
+done < "$SUMFILE"

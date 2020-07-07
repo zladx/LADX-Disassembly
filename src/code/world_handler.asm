29fb531b18b803614a82c386c588763727a378f1
Xkeeper <xkeeper@gmail.com>
2020-07-07 01:48:31 -0700

    Add small note to world_handler

--- src/code/world_handler.asm
+++ src/code/world_handler.asm
@@ -36,6 +36,7 @@ GameplayWorldSubtype0Handler::
     call label_27F2                               ; $4395: $CD $F2 $27
     call IncrementGameplaySubtype                 ; $4398: $CD $D6 $44
 IF !__PATCH_5__
+    ; POI: This is where debug flag 2 messes up the subscreen
     ld   a, [ROM_DebugTool2]                      ; $439B: $FA $04 $00
     and  a                                        ; $439E: $A7
     jr   z, jr_001_43A7                           ; $439F: $28 $06

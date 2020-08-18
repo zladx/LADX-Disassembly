8dcc34c63005690e18a74cf3c2599e77360202e4
zelosos <oneiroi@posteo.de>
2020-08-19 00:59:25 +0200

    add label guideline for hRAM

--- src/constants/memory/hram.asm
+++ src/constants/memory/hram.asm
@@ -1,5 +1,20 @@
 section "HRAM", HRAM[$ff80]
 
+; *******************************************************************
+; *                                                                 *
+; *             >> LABEL GUIDELINE <<                               *
+; *                                                                 *
+; *         ; Description of the usage of this memory address.      *
+; *         ; Possible values:                                      *
+; *         ; 0 = meaning 0,                                        *
+; *         ; 1 = meaning 1,                                        *
+; *         ; 2 = meaning 2                                         *
+; *         label::                                                 *
+; *           ds 1 ; address as 4 hex value                         *
+; *                                                                 *
+; *******************************************************************
+
+
 hRomBank:: ; FF80
  ds 1
 

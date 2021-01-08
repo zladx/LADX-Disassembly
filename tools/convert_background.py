2670a4b486dc6c3b33d4c76d9848c8d15198914e
Pierre de La Morinerie <kemenaran@gmail.com>
2021-01-08 23:29:16 +0000

    tools: fix tilemap encoding
    TODO: for know only tilemaps starting at 0x9800 with a width of 20 can
    be correctly encoded.

--- tools/convert_background.py
+++ tools/convert_background.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     operation_args = arg_parser.add_argument_group('Operation').add_mutually_exclusive_group()
     operation_args.add_argument('--decode', '-d', nargs=1, type=str, metavar='encoded tilemap', help='encoded binary tilemap file to decode')
     operation_args.add_argument('--encode', '-e', nargs=1, type=str, metavar='tilemap', help='raw binary tilemap file to encode')
-    arg_parser.add_argument('--wrap', '-w', nargs=1, type=int, metavar='char_count', default=40, help='wrap the stdout output to a number of characters (40 by default; 0 to disable)')
+    arg_parser.add_argument('--wrap', '-w', nargs=1, type=int, metavar='char_count', default=[40], help='wrap the stdout output to a number of characters (40 by default; 0 to disable)')
     arg_parser.add_argument('--output', '-o', nargs=1, type=str, metavar='outfile', help='file to write the output to')
 
     args = arg_parser.parse_args()
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         arg_parser.print_help()
         sys.exit(-1)
 
-    infile = open(args.decode[0] or args.encode[0], 'rb')
+    infile = open((args.decode and args.decode[0]) or args.encode[0], 'rb')
     data = infile.read()
     infile.close()
 
@@ -49,6 +49,8 @@ if __name__ == "__main__":
         write_result(result, outfile, wrap_count=args.wrap[0])
 
     elif args.encode:
+        # TODO: allow to specify the tilemap location and width from command-line argument
+        # (For now defaults of location=0x9800 and width=20 are assumed.)
         result = BackgroundCoder.encode(data)
         write_result(result, outfile, wrap_count=args.wrap[0])
 

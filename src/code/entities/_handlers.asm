7b078db7f028b086c664e97081bcf540ea97bbe9
Pierre de La Morinerie <kemenaran@gmail.com>
2022-12-16 10:34:22 +0000

    src: attempt to document entity 8C and 8D

--- src/code/entities/_handlers.asm
+++ src/code/entities/_handlers.asm
@@ -153,8 +153,8 @@ EntityHandlersTable::
 ._89 far_pointer HinoxEntityHandler
 ._8A far_pointer TileGlintShownEntityHandler
 ._8B far_pointer TileGlintHiddenEntityHandler
-._8C far_pointer Entity8CHandler ; $8C
-._8D far_pointer Entity8DHandler
+._8C far_pointer Entity8CHandler ; unused moving block?
+._8D far_pointer Entity8DHandler ; unused moving block?
 ._8E far_pointer CueBallEntityHandler
 ._8F far_pointer MaskedMimicGoriyaEntityHandler
 ._90 far_pointer ThreeOfAKindEntityHandler ; $90
